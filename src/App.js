4f8d3ac9cb8c6efa240ef775f7044e442725c76f
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-08-11 10:59:06 +0400

    Temp hide lazy component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { UsersPage } from './pages/UsersPage/index';
+import { HomePage } from './pages/HomePage';
+import { UsersPage } from './pages/UsersPage';
 import './App.scss';
 import { Layout } from './components/Layout/index';
 import { Spinner } from './components/Spinner';
 
-const HomePage = React.lazy(() => import('./pages/HomePage'));
+// const HomePage = React.lazy(() => import('./pages/HomePage'));
+// const UsersPage = React.lazy(() => import('./pages/UsersPage/index'));
 
 function App() {
   React.useCallback(() => onClick, [])
@@ -19,10 +21,11 @@ function App() {
         <Layout>
             <React.Suspense fallback={<Spinner />}>
                 <Routes>
-                    {/* <Route path="/" element={<HomePage onClick={onClick} />} /> */}
-                    <Route path="/" component={HomePage} />
+                    <Route path="/" element={<HomePage onClick={onClick} />} />
+                    {/* <Route path="/" component={HomePage} /> */}
                     <Route path="/users" element={<UsersPage />} />
-                    <Route path="/users/:userId" element={<div>personal user page</div>} />
+                    {/* <Route path="/users" component={UsersPage} /> */}
+                    {/* <Route path="/users/:userId" element={<div>personal user page</div>} /> */}
                 </Routes>
             </React.Suspense>
         </Layout>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { LandingComponent } from './components/LandingComponent/index'
+// import { LandingComponent } from './components/LandingComponent/index'
+const LandingComponent = React.lazy(() => import('./components/LandingComponent/index'));
 
 const HomePage = (props) => {
   return (
     <div onClick={props.onClick}>
-      <LandingComponent />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <LandingComponent />
+      </React.Suspense>
     </div>
   );
 }
